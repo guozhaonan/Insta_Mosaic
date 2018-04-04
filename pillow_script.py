fc5216a3831ca37761b0801acac822d79f90fba1
Albert Guo <guozhaonan@gmail.com>
2018-04-03 20:55:22 -0400

    Added 3x3 Square Functionality

--- pillow_script.py
+++ pillow_script.py
@@ -3,7 +3,10 @@ from PIL import Image
 # The following are test images, in order to make use of this for your own images, save your images to a variable like below
 
 #Access Image 1
-im_two = Image.open('input_photos/test2.JPG')
+im_one = Image.open('input_photos/test1.jpg')
+
+#Access Image 2
+im_two = Image.open('input_photos/test2.jpg')
 
 #Access Image 3
 im_three = Image.open('input_photos/test3.jpg')
@@ -43,5 +46,27 @@ def split_images(image, name):
             low[position].paste(image.crop((left, six_square_size[0], right, six_square_size[0]*2)))
             up[position].save('output_photos/'+name+str(position)+'.jpeg', 'jpeg')
             low[position].save('output_photos/'+name+str(position+3)+'.jpeg', 'jpeg')
-
-split_images(im_two, "six_squares")
+    elif height == width:
+        square_square = (height/3, width/3)
+        upper_left = Image.new('RGB', square_square)
+        upper_middle = Image.new('RGB', square_square)
+        upper_right = Image.new('RGB', square_square)
+        middle_left = Image.new('RGB', square_square)
+        middle_middle = Image.new('RGB', square_square)
+        middle_right = Image.new('RGB', square_square)
+        lower_left = Image.new('RGB', square_square)
+        lower_middle = Image.new('RGB', square_square)
+        lower_right = Image.new('RGB', square_square)
+        up = [upper_left, upper_middle, upper_right]
+        middle =[middle_left, middle_middle, middle_right]
+        low = [lower_left, lower_middle, lower_right]
+        for position in range(0, 3):
+            left = position*square_square[0]
+            right = (position + 1)*square_square[0]
+            up[position].paste(image.crop((left, 0, right, square_square[0])))
+            middle[position].paste(image.crop((left, square_square[0], right, square_square[0]*2)))
+            low[position].paste(image.crop((left, square_square[0]*2, right, square_square[0]*3)))
+            up[position].save('output_photos/'+name+str(position)+'.jpeg', 'jpeg')
+            middle[position].save('output_photos/'+name+str(position+3)+'.jpeg', 'jpeg')
+            low[position].save('output_photos/'+name+str(position+6)+'.jpeg', 'jpeg')
+split_images(im_one, "square_squares")
